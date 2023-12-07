246c4b8a52262cb19841f111323b4954930f83d9
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-07 17:18:40 -0300

    updated proj: add colors in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
    theme: {
+      colors: {
+         // primary
+         activeInput: 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+         errorInput: 'hsl(0, 100%, 66%)',
+
+         // neutral
+         white: 'hsl(0, 0%, 100%)',
+         lightGrayshViolet: 'hsl(270, 3%, 87%)',
+         darkGrayshViolet: 'hsl(279, 6%, 55%)',
+         veryDarkViolet: 'hsl(278, 68%, 11%)',
+      },
       extend: {},
    },
    plugins: [],
