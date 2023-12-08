4a47c7bedd0031556252e593f002213cb8fbfba6
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-08 18:13:49 -0300

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
 import React from 'react';
 import Input from './Components/Input';
+import Cards from './Components/Cards';
 
 const inputDefaultCSS = {
    def01: 'border border-lightGrayshViolet rounded',
-};
-
-const imgCard = {
-   back: 'bg-center bg-thousand bg-no-repeat w-96 h-52',
 };
 
 function App() {
    return (
       <main className="font-spaceGrotesk text-lg border h-screen bg-mainDesktop bg-custom bg-no-repeat">
          <section className="flex mx-auto translate-y-2/4 justify-center gap-40 items-center w-[1200px]">
-            <div className="flex flex-col gap-8">
-               <div
-                  className={`bg-cardFront px-7 py-5 flex flex-col justify-end relative ${imgCard.back}`}
-               >
-                  <img
-                     className="absolute left-7 top-5"
-                     src="../public/svg/card-logo.svg"
-                     alt=""
-                  />
-
-                  <div className="text-white">
-                     <h1 className="mb-3 text-2xl tracking-widest">
-                        0000 0000 0000 0000
-                     </h1>
-                     <div className="flex justify-between uppercase text-base items-center">
-                        <span>Person name</span>
-                        <span>00/00</span>
-                     </div>
-                  </div>
-               </div>
-               <div className={`bg-cardBack ml-16 relative ${imgCard.back}`}>
-                  <span className="absolute top-[5.75rem] left-[19.5rem] text-white text-sm">
-                     000
-                  </span>
-               </div>
-            </div>
+            <Cards />
             <form className="w-fit">
                <div>
                   <Input
