7f60b2006338b3b5c7cb9837896d3378d4007de1
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-14 15:13:15 -0300

    updated button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Button = () => {
-  return (
-    <button className="w-full bg-veryDarkViolet py-3 mt-10 rounded-md cursor-pointer text-white hover:bg-opacity-90">
-      Confirm
-    </button>
-  );
+   return (
+      <button className="w-full bg-veryDarkViolet py-3 mt-10 rounded-md cursor-pointer text-white hover:bg-opacity-90 ss:mt-5">
+         Confirm
+      </button>
+   );
 };
 
 export default Button;
